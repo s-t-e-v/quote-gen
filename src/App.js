dc8c6e9e366141705b9f62491375c48de9d5c534
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-17 17:35:40 +0200

    css + html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './style.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Project 3: Quote Generator</h1>
+
+      <div className="quote-generator">
+        <button>New quote</button>
+        <p className="quote">"The show must go on!"</p>
+        <p className="author">- Anonymous</p>
+      </div>
     </div>
   );
 }
